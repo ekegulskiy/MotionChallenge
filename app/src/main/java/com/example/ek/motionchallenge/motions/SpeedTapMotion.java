ac4c66d8e9a0fb86095155c8b18af7e9830f6225
Eduard Kegulskiy <ekegulsk@mail.sfsu.edu>
2017-12-03 12:31:03 -0800

    #14 Implement Speed Tap motion

---
+++ app/src/main/java/com/example/ek/motionchallenge/motions/SpeedTapMotion.java
@@ -0,0 +1,33 @@
+package com.example.ek.motionchallenge.motions;
+
+import android.content.Context;
+
+/**
+ * Created by ek on 12/3/17.
+ */
+
+public class SpeedTapMotion extends MotionBase {
+    private final String TAG="speedTapMotion";
+    private final String NAME="speedTap";
+    private final int MOTION_DURATION_SEC = 5;
+
+    public SpeedTapMotion(Context context){
+        super(context);
+        setMotionDuration(MOTION_DURATION_SEC);
+    }
+
+    @Override
+    public void onMotionEnd() {
+
+    }
+
+    @Override
+    public void onMotionStart(){
+
+    }
+
+    @Override
+    public String getMotionName() {
+        return NAME;
+    }
+}
--- app/src/main/java/com/example/ek/motionchallenge/ui/MotionScreen.java
+++ app/src/main/java/com/example/ek/motionchallenge/ui/MotionScreen.java
@@ -23,6 +23,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -33,6 +34,7 @@ import com.example.ek.motionchallenge.R;
 import com.example.ek.motionchallenge.motions.JumpUpMotion;
 import com.example.ek.motionchallenge.motions.MotionBase;
 import com.example.ek.motionchallenge.motions.ShakeMotion;
+import com.example.ek.motionchallenge.motions.SpeedTapMotion;
 import com.example.ek.motionchallenge.motions.Swing360Motion;
 
 public class MotionScreen extends AppCompatActivity {
@@ -44,6 +46,7 @@ public class MotionScreen extends AppCompatActivity {
     private MediaPlayer mMediaPlayer;
     private TextView mFeedbackView;
     private FirebaseScoresDB mScoresDB;
+    private int mMotionID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +56,7 @@ public class MotionScreen extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Bundle b = getIntent().getExtras();
-        int motionID = b.getInt("motionID");
+        mMotionID = b.getInt("motionID");
 
         mMotionDescView = findViewById(R.id.MotionScreen_descView);
         mStartBtn = findViewById(R.id.MotionScreen_StartBtn);
@@ -65,7 +68,7 @@ public class MotionScreen extends AppCompatActivity {
 
         mScoresDB = FirebaseScoresDB.getInstance();
 
-        switch(motionID){
+        switch(mMotionID){
             case R.id.shakeMotionIconView:
                 mMotionDescView.setText(R.string.home_screen_shake_motion_desc);
                 mMotion = new ShakeMotion(this);
@@ -83,6 +86,14 @@ public class MotionScreen extends AppCompatActivity {
 
             case R.id.speedTapMotionIconView:
                 mMotionDescView.setText(R.string.home_screen_speedtap_motion_desc);
+                mMotion = new SpeedTapMotion(this);
+
+                mMotionProgressBar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mMotion.incrementMotionCount();
+                    }
+                });
                 break;
 
             default:
@@ -128,4 +139,5 @@ public class MotionScreen extends AppCompatActivity {
         mMediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.beep1);
         mMotionProgressBar.getIndeterminateDrawable().setColorFilter(0xFF33b5E5, android.graphics.PorterDuff.Mode.MULTIPLY);
     }
+
 }
