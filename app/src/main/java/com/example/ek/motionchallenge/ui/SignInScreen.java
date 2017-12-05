ec7d6aff3724f6609ed8d7fdb21b385b40b1ee2a
Eduard Kegulskiy <ekegulsk@mail.sfsu.edu>
2017-12-05 06:49:51 -0800

    Display a small toast to the user when no network connection detected

--- app/src/main/java/com/example/ek/motionchallenge/ui/SignInScreen.java
+++ app/src/main/java/com/example/ek/motionchallenge/ui/SignInScreen.java
@@ -34,7 +34,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -119,6 +121,11 @@ public class SignInScreen extends AppCompatActivity implements
                 firebaseAuthWithGoogle(account);
             } else {
                 Log.e(TAG, "Google Sign In failed.");
+                if(result.getStatus().getStatusCode() == CommonStatusCodes.NETWORK_ERROR)
+                {
+                    Log.e(TAG, "Sign In failed due to network connection.");
+                    Toast.makeText(this, "Network connection failed", Toast.LENGTH_SHORT).show();
+                }
             }
         }
     }
